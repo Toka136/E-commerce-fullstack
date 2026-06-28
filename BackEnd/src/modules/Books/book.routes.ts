cf6d0d9cc9fc5d6e8eb02a2267d2daa5dde74e15
toka136 <tokakaram212@gmail.com>
2026-06-28 11:31:24 +0300

    feat(books): Implement CRUD operations for books

--- BackEnd/src/modules/Books/book.routes.ts
+++ BackEnd/src/modules/Books/book.routes.ts
@@ -1,8 +1,13 @@
 import express from "express"
 import Wrapper from "../../MiddleWares/Wrapper"
-import { addBook_C } from "./book.controller"
+import { addBook_C, deleteBook_C, getBook_C, getBooks_C, updateBook_C } from "./book.controller"
 import { imageUpload } from "../../utils/multerCode"
 import { adminPermision } from "../../MiddleWares/adminPermision"
 const router=express.Router()
 router.route("/addBook").post(adminPermision,imageUpload.single("coverImage"),Wrapper(addBook_C))
+router.route("/updateBook").patch(adminPermision,imageUpload.single("coverImage"),Wrapper(updateBook_C))
+router.route("/deleteBook/:id").delete(adminPermision,Wrapper(deleteBook_C))
+router.route("/geteBook/:id").get(Wrapper(getBook_C))
+router.route("/geteBooks/").get(Wrapper(getBooks_C))
+
 export default router
--- BackEnd/src/modules/Books/book.types.ts
+++ BackEnd/src/modules/Books/book.types.ts
@@ -1,4 +1,4 @@
-export type bookT={
+export interface bookI{
     title:string,
     author:string,
     genre:string,
@@ -6,4 +6,19 @@ export type bookT={
     description:string,
     coverImage:string,
     stock:number
+}
+export interface editBookI{
+    title?:string,
+    author?:string,
+    genre?:string,
+    price?:number,
+    description?:string,
+    coverImage?:string,
+    stock?:number,
+    id:string
+}
+export interface queryI{
+    searchText?:string,
+    pageSize?:number,
+    currentPage?:number
 }
